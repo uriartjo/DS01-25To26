8e702b94842c78a94d4e6909aab30096d8248703
Joey <vespar@gmail.com>
2025-09-08 13:06:11 -0400

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Why did the private classes break up?");
+        System.out.println("Because they never saw each other.");
 
+        System.out.println();
+
+        System.out.println("Why did the Java developer teach his young kids about single quotes?");
+        System.out.println("Because they build character.");
     }
 }
